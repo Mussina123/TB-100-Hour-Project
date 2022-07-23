7786a5726ef0ac307121b1f7a1651aba4674df18
Tyler Beck <softwaretpd2035@gmail.com>
2022-07-23 19:37:52 -0400

    running node.js server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname, 'index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Connected to Port ${PORT}`)
+})
