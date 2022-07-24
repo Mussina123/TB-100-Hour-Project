b2df330d0d6079615bd338f1a605b80a9ec0c4c0
Tyler Beck <softwaretpd2035@gmail.com>
2022-07-23 20:51:30 -0400

    added .env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 const cors = require('cors')
 app.use(cors())
+
 
 const PORT = process.env.PORT || 5000
 
