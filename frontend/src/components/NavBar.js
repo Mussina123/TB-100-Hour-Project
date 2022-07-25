ae1d046f5548cce8aa65aa034fc87797b47f492e
Tyler Beck <softwaretpd2035@gmail.com>
2022-07-25 09:40:19 -0400

    working map to display from DB

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
     return (
        <header>
-         <div className="contianer">
+         <div className="contianer text-4xl pl-10 bg-sky-200 h-20 font-extrabold pt-4">
             <Link to='/'>
-                <h1>Food Diary Log</h1>
+                <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Food Diary Log</h1>
             </Link>
         </div>
        </header>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+
+    const [diary, setDiary] = useState([])
+
+    useEffect(() => {
+        const fetchDiary = async () => {
+            const response = await fetch('http://localhost:5000/api/diary') 
+            const json = await response.json()
+
+
+            if(response.ok) {
+                setDiary(json) // gives back an array of json data 
+                console.log(json)
+            }
+
+        }
+        
+        fetchDiary()
+    }, [])
+
     return(
         <div className="home">
-            <h2>Home</h2>
+            <div>
+                {diary.map((diary) => (
+                    <h1 className="text-2xl"  key={diary._id}>{diary.item}</h1>
+                ))}
+            </div>
         </div>
     )
 }
