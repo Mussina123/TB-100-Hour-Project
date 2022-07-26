39920bdb55755d8b86b77d6dfebc52ee64599a22
Tyler Beck <softwaretpd2035@gmail.com>
2022-07-25 21:37:56 -0400

    added tailwind styling for frontend

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
        <header>
          <div className="contianer text-4xl pl-10 bg-sky-200 h-20 font-extrabold pt-4">
             <Link to='/'>
-                <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">Food Diary Log</h1>
+                <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-violet-800">Food Diary Log</h1>
             </Link>
         </div>
        </header>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -24,7 +24,11 @@ const Home = () => {
         <div className="home">
             <div>
                 {diary.map((diary) => (
-                    <h1 className="text-2xl"  key={diary._id}>{diary.item}</h1>
+                    <div className="flex flex-col ml-6 mt-6 shadow-lg shadow-slate-500/20 w-40 rounded-xl" >
+                        <h1 className="text-3xl font-semibold"  key={diary._id}> {diary.item}</h1>
+                        <h3 className="italic"> Calories: {diary.calories}</h3>
+                        <h3 className="text-slate-600"> Serving: {diary.serving}</h3>
+                    </div>
                 ))}
             </div>
         </div>
