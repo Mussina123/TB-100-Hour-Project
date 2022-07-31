1930a14519af985f8db9357ad341b3021be9ed02
Tyler Beck <softwaretpd2035@gmail.com>
2022-07-31 10:50:52 -0400

    changing styling

--- frontend/src/components/DiaryDetails.js
+++ frontend/src/components/DiaryDetails.js
@@ -20,10 +20,10 @@ const DiaryDetails = ({diary}) => {
 
     return (
         <div className="flex flex-col m-auto md:ml-6 mt-6 shadow-lg shadow-slate-500/20 rounded-xl relative w-60" >
-            <h1 className="text-3xl font-semibold mb-2" key={diary._id}> {diary.item}</h1>
-            <h3 className="italic mb-1"> Calories: {diary.calories}</h3>
-            <h3 className="text-slate-600 mb-1"> Serving: {diary.serving}</h3>
-            <h3 className="text-slate-600"> {formatDistanceToNow(new Date(diary.createdAt), { addSuffix: true })}</h3>
+            <h1 className="text-3xl font-semibold pt-2 pl-2" key={diary._id}> {diary.item}</h1>
+            <h3 className="italic  pt-2 pl-2"> Calories: {diary.calories}</h3>
+            <h3 className="text-slate-600 pt-2 pl-2"> Serving: {diary.serving}</h3>
+            <h3 className="text-slate-600 pt-2 pl-2"> {formatDistanceToNow(new Date(diary.createdAt), { addSuffix: true })}</h3>
             <span className="material-symbols-outlined cursor cursor-pointer absolute top-2 right-2" onClick={handleClick}>Delete</span>
     </div>
     )
